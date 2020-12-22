3005ff0ff6d9fbaddfc99bac63533baf63c4cfe2
Janek Szynal <jan.szynal@gmail.com>
2020-12-22 15:23:59 +0100

    Initial Commit

---
+++ JoyOfPainting/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JoyOfPainting
+//
+//  Created by Janek Szynal on 22.12.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JoyOfPainting/JoyOfPaintingApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoyOfPaintingApp.swift
+//  JoyOfPainting
+//
+//  Created by Janek Szynal on 22.12.20.
+//
+
+import SwiftUI
+
+@main
+struct JoyOfPaintingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
