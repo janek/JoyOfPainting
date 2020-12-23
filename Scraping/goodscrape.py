7946d47c5df04321d47d5b9b9c635b7d973d46c3
Janek Szynal <jan.szynal@gmail.com>
2020-12-23 00:38:28 +0000

    Update scraping script

--- Scraping/goodscrape.py
+++ Scraping/goodscrape.py
@@ -10,8 +10,8 @@ base_season_url = "https://www.twoinchbrush.com/season/"
 
 dfs = []
 
-num_seasons = 31 # 31
-for i in range(1,num_seasons):
+num_seasons = 31 
+for i in range(1,num_seasons+1):
 	url =  base_season_url + str(i)
 	page = requests.get(url)
 	soup = BeautifulSoup(page.content, 'html.parser')
@@ -35,7 +35,9 @@ for i in range(1,num_seasons):
 	dfs.append(episodes_df)
 
 
-df = pd.concat([df for df in dfs])
-df = df[df.image_link.str.contains("painting")]
+episodes = pd.concat([df for df in dfs])
+episodes = episodes[episodes.image_link.str.contains("painting")]
+episodes = episodes[episodes.image_link != "so-you-want-to-buy-a-bob-ross-painting.jpg"]
+episodes.reset_index(drop=True, inplace=True)
 
-df.to_csv("episodes.csv")
+episodes.to_csv("episodes.csv")
