4746051cbf5ccb2300d5866d1ae078c1603de96e
Janek Szynal <jan.szynal@gmail.com>
2020-12-23 22:24:07 +0100

    rename columns in cleanup_and_merge for clarity

--- Scraping/cleanup_and_merge.py
+++ Scraping/cleanup_and_merge.py
@@ -5,9 +5,9 @@ import pandas as pd
 
 # XXX: file paths
 youtube_episodes = pd.read_csv("episodes_from_youtube.csv", names=["episode_title", "video_id"])
+youtube_episodes["episode_signature"] = ""
 youtube_episodes["episode_number"] = ""
-youtube_episodes["e"] = ""
-youtube_episodes["s"] = ""
+youtube_episodes["season_number"] = ""
 
 
 for i, row in youtube_episodes.iterrows():
@@ -34,13 +34,14 @@ for i, row in youtube_episodes.iterrows():
 	signature = "S" + season_number + "E" + episode_number
 
 	row["episode_title"] = title
-	row["episode_number"] = signature
-	row["e"] = int(episode_number)
-	row["s"] = int(season_number)
+	row["episode_signature"] = signature
+	row["episode_number"] = int(episode_number)
+	row["season_number"] = int(season_number)
 
 web_episodes = pd.read_csv("episodes_from_web.csv")
 
 
-episodes = pd.merge(youtube_episodes, web_episodes, how="outer", on=["episode_number", "episode_title"])
-episodes.sort_values(["s", "e"], inplace=True)
-episodes.to_csv("episodes.csv")
+
+episodes = pd.merge(youtube_episodes, web_episodes, how="inner", on=["episode_signature", "episode_title"])
+episodes.sort_values(["season_number", "episode_number"], inplace=True)
+episodes.to_csv("episodes_final.csv")
